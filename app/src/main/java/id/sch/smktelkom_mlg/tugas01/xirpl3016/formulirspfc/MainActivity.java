d7fe48c9e5a06f1b92fd98b624f6c1fca0bf7157
Leovano Adias <aleovano@gmail.com>
2016-09-11 22:10:43 +0700

    Pemberian Validasi Pada Nama, Email, dan Tahun Kelahiran

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3016/formulirspfc/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3016/formulirspfc/MainActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3016.formulirspfc;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -26,19 +27,65 @@ public class MainActivity extends AppCompatActivity {
         etNama = (EditText) findViewById(R.id.editTextNama);
         etTahun = (EditText) findViewById(R.id.editTextTahun);
         bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        rbLK = (RadioButton) findViewById(R.id.radioButtonLK);
+        rbPR = (RadioButton) findViewById(R.id.radioButtonPR);
 
-        /*bOK.setOnClickListener(new View.OnClickListener() {
+        bOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
                 doClick();
             }
-        });*/
+        });
     }
 
     private void doClick() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            tvHasil.setText("Terima Kasih " + nama + " atas partisipasi anda");
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
         String nama = etNama.getText().toString();
-        tvHasil.setText("Terimasih");
+        String tahun = etTahun.getText().toString();
+        String email = etEmail.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum Diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama Minimal 3 Karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        if (tahun.isEmpty()) {
+            etTahun.setError("Tahun Kelahiran Belum Diisi");
+            valid = false;
+        } else if (tahun.length() != 4) {
+            etTahun.setError("Format Tahun Kelahiran Bukan yyyy");
+            valid = false;
+        } else {
+            etTahun.setError(null);
+        }
+
+        if (email.isEmpty()) {
+            etEmail.setError("Email Belum Diisi");
+            valid = false;
+        } else if (email.length() < 7) {
+            etEmail.setError("Pastikan format email benar");
+            valid = false;
+        } else {
+            etEmail.setError(null);
+        }
+
+
+        return valid;
     }
 
 }
