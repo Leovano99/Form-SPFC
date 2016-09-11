664e59c9922f75b3afe6024732b55449f7cf3b96
Leovano Adias <aleovano@gmail.com>
2016-09-11 21:18:14 +0700

    Penambahan Image, Text, Edit Text, Spinner, Radio Button, Checkbox,  Button, dan Scroll View

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3016/formulirspfc/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3016/formulirspfc/MainActivity.java
@@ -2,8 +2,12 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3016.formulirspfc;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama, etTahun, etEmail;
+    CheckBox cbBK, cbBR, cbGL, cbKR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
